cfaffe1bbe3f026de215b3644f1c8801b6fc3429
Michael Harmon and Talia Haller <michael+talia@>
2018-09-13 16:47:02 -0700

    final commit

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,15 @@ $(document).ready(function(){
   $(".svgDiv,.svgDivHidden").click(function(){
     $(".sideBarWrapper").toggle();
   })
+  $(".lessons li").click(function(){
+    $(".lessonContent div").hide();
+    var clickValue = '.' + this.className;
+    console.log(clickValue);
+    $(clickValue).show();
+    var progLength = parseInt($(this).siblings().length) + 1
+
+    var progBarFill = 100/progLenth * i
+    $(".progress").html(' <div class="progress-bar bg-info" role="progressbar" style="width: 25%" <span aria-valuenow="' + progBarFill + '" aria-valuemin="0" aria-valuemax="100"></div>')
+  })
+
 })
